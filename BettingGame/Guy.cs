9fe779beb9e3a1f6ef574af6d0fc2a5aa623e90b
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-06-28 18:08:30 +0100

    Add project files.

---
+++ BettingGame/Guy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BettingGame
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        /// <summary>
+        /// Writes name and amount of cash to the console.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + "$" + Cash + ".");
+        }
+
+        /// <summary>
+        /// Gives some cash, removes from wallet (or prints message to console if insuffient funds).
+        /// </summary>
+        /// <param name="amount">Amount of cash to give.</param>
+        /// <returns>Amount of cash removed from wallet. Otherwise, 0 if insufficient funds or invalid amount.</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + "$" + amount + " isn't a valid amount.");
+                return 0;
+            }
+
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " + "I don't have enough cash to give you " + "$" + amount);
+                return 0;
+            }
+
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Receive an amount of cash, adds it to wallet (or prints message to console if invalid amount).
+        /// </summary>
+        /// <param name="amount">Cash amount to give.</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + "$" + amount + " isn't an amount I'll take.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ BettingGame/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BettingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double odds = 0.75;
+
+            Guy player = new Guy()
+            {
+                Name = "The player", Cash = 100
+            };
+
+            Console.WriteLine("Welcome to the ZomboCom Casino. The odds here are 0.75");
+
+            while (player.Cash > 0)
+            {
+                Console.WriteLine(player.Name + " has " + "$" + player.Cash);
+
+                Console.Write("Please input how much you want to bet: ");
+                string howMuch = Console.ReadLine();
+                int.TryParse(howMuch, out int amount);
+
+                if (amount > player.Cash)
+                {
+                    Console.WriteLine("Not enough CASH there stranger. The house is watching you...");
+                }
+
+                else
+                {
+                    int pot = amount * 2;
+
+                    double rng = random.NextDouble();
+
+                    if (rng > odds)
+                    {
+                        Console.WriteLine("Huzzah, it's a win of " + "$" + pot + " for you!");
+                        player.Cash += pot;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You lose " + "$" + amount + "." + " Bad luck, friend.");
+                        player.Cash -= amount;
+                    }
+                }
+  
+            }
+
+            Console.WriteLine("Game over. The house wins all.");
+            string gameReset = Console.ReadLine();
+            if (gameReset == "Restart")
+            {
+                
+            }
+
+        }
+    }
+}
